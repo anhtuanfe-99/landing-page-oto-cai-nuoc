f3f761f92e3546b19633419ace7481fd3fda1be7
macos <nguyenanhtuan.220499@gmail.com>
2025-09-14 16:11:00 +0700

    Update Next.js configuration and replace image assets with WebP format; refactor HomePage layout for improved structure and performance

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+	images: {
+		formats: ["image/webp", "image/avif"],
+		minimumCacheTTL: 31536000, // 1 year
+	},
+	compress: true,
+	poweredByHeader: false,
+};
 
 export default config;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,21 +3,21 @@ import "~/styles/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Dịch vụ xe du lịch Cái Nước - Đặt xe du lịch Cái Nước",
-  description:
-    "Dịch vụ xe du lịch tại Cái Nước, mang đến trải nghiệm an toàn, thoải mái và giá ưu đãi. Xe đời mới, sạch sẽ, bảo dưỡng định kỳ cùng đội ngũ tài xế chuyên nghiệp, phục vụ tận tình mọi lúc mọi nơi.",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+	title: "Dịch vụ xe du lịch Cái Nước - Đặt xe du lịch Cái Nước",
+	description:
+		"Dịch vụ xe du lịch tại Cái Nước, mang đến trải nghiệm an toàn, thoải mái và giá ưu đãi. Xe đời mới, sạch sẽ, bảo dưỡng định kỳ cùng đội ngũ tài xế chuyên nghiệp, phục vụ tận tình mọi lúc mọi nơi.",
+	icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en" className={`${inter.className}`}>
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en" className={`${inter.className}`}>
+			<body>{children}</body>
+		</html>
+	);
 }
